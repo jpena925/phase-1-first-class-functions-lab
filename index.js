cdebba5d0f1006e85eb2744c83d6575c18559b52
Jack Pena <penajackc@gmail.com>
2022-01-10 17:02:02 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (drivers) => drivers.slice(0,2);
+
+const returnLastTwoDrivers = (drivers) => drivers.slice(-2);
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(num){
+    return function(fare){
+        return fare * num;
+    }
+};
+
+const fareDoubler = function(fare){
+    return createFareMultiplier(2)(fare);
+}
+
+const fareTripler = function(fare){
+    return createFareMultiplier(3)(fare);
+}
+
+function selectDifferentDrivers(drivers, cb){
+    return cb(drivers);
+}
